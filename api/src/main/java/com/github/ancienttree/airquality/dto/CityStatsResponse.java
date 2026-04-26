2318ffce6ab0f374ac6612e1cbe0f402b907848e
Mateusz Dąbek <mate.dab@gmail.com>
2026-04-26 13:28:11 +0200

    (api): round city statistics

--- api/src/main/java/com/github/ancienttree/airquality/dto/CityStatsResponse.java
+++ api/src/main/java/com/github/ancienttree/airquality/dto/CityStatsResponse.java
@@ -16,4 +16,21 @@ public record CityStatsResponse(
         Double maxPM10,
         Double minPM10
 ) {
+    public CityStatsResponse {
+        avgNO2 = round(avgNO2);
+        maxNO2 = round(maxNO2);
+        minNO2 = round(minNO2);
+
+        avgCO = round(avgCO);
+        maxCO = round(maxCO);
+        minCO = round(minCO);
+
+        avgPM10 = round(avgPM10);
+        maxPM10 = round(maxPM10);
+        minPM10 = round(minPM10);
+    }
+
+    private static double round(double value) {
+        return Math.round(value * 100.0) / 100.0;
+    }
 }
